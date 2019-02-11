fe3f195e2b0a9dceca01b7bb2f91ad22d8082c84
James <jwdeharo@gmail.com>
2019-02-11 20:35:28 +0100

    First open gl commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,80 @@
-#include "Math/Vector3.h"
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include "Utils/Utils.h"
+//Window dimensions.
+const GLint WIDTH   = 800;
+const GLint HEIGHT  = 600;
+
+constexpr int ERROR   = 1;
+constexpr int SUCCESS = 0;
 
 int main()
 {
   using namespace EphemeralEngine;
-
-  Maths::Vector3 A(3.f, 5.f, 7.f);
-  Maths::Vector3 B(11.f, 13.f, 17.f);
   
+  //Initialise GLFW
+  if (!glfwInit())
+  {
+    glfwTerminate();
+    Utils::fatal("Couldn't initialise GLFW");
+    return ERROR;
+  }
 
-  Utils::dbg("Vector is %f %f %f", A.Cross(B).x, A.Cross(B).y, A.Cross(B).z);
+  //Setup GLFW window properties.
+  //OPENGL version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //Core profile = No backwards compatibility.
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  //Allow forward compatibility.
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  return -1;
+  GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "EphemeralEngine", nullptr, nullptr);
+
+  if (mainWindow == nullptr)
+  {
+    glfwTerminate();
+    Utils::fatal("GLFW window creation failed.");
+    return ERROR;
+  }
+
+  //Get buffer size information.
+  int bufferWidth;
+  int bufferHeight;
+
+  glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+  //Set the context for GLEW to use.
+  glfwMakeContextCurrent(mainWindow);
+
+  //Allow modern extension features.
+  glewExperimental = GL_TRUE;
+
+  if (glewInit() != GLEW_OK)
+  {
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    Utils::fatal("Couldn't initialise GLEW.");
+    return ERROR;
+  }
+
+  //Setup viewport size.
+  glViewport(0, 0, bufferWidth, bufferHeight);
+
+  //Loop until window closed.
+  while (!glfwWindowShouldClose(mainWindow))
+  {
+    //Get and handle user input events.
+    glfwPollEvents();
+
+    //Clear window.
+    glClearColor(1.0f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(mainWindow);
+  }
+
+  return SUCCESS;
 }
