cc811fd4b8f34369b420ff0661e1c3bfab572cdc
James <jwdeharo@gmail.com>
2019-02-14 21:23:36 +0100

    Beginning of the ECS

---
+++ source/ECS/Entity.h
@@ -0,0 +1,15 @@
+#ifndef _ENTITY_H
+#define _ENTITY_H
+
+namespace EphemeralEngine
+{
+  namespace Entities
+  {
+    struct Entity
+    {
+      unsigned int id; //!< Id to identify each entity.
+    };
+  }
+}
+
+#endif
---
+++ source/ECS/EntityManager.h
@@ -0,0 +1,17 @@
+#ifndef _ENTITY_MANAGER_H
+#define _ENTITY_MANAGER_H
+
+namespace EphemeralEngine
+{
+  namespace Entities
+  {
+    struct Entity;
+
+    class EntityManager
+    {
+
+    };
+  }
+}
+
+#endif
