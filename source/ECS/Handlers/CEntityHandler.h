c31de05a433ce03bf8a72e98d245f833f3e73413
James <jwdeharo@gmail.com>
2019-02-19 22:20:38 +0100

    Entity handler creation

---
+++ source/ECS/Handlers/CEntityHandler.h
@@ -0,0 +1,31 @@
+#ifndef _CENTITY_HANDLER
+#define _CENTITY_HANDLER
+
+#include "../CEntity.h"
+#include "../CWorld.h"
+
+namespace EphemeralEngine
+{
+  namespace Entities
+  {
+    struct CEntityHandler
+    {
+      CWorld* mpWorld; //!< Pointer to the world.
+      CEntity mEntity; //!< Entity to handle.
+
+      /**
+      * Template functions that allows us to add a component to an entity.
+      * @param CEntity -> Entity where we want to add the component.
+      * @param T -> Component that we want to add.
+      */
+      template<typename T>
+      void AddComponent(T& aComponent)
+      {
+        mpWorld->AddComponent<T>(mEntity, aComponent);
+      }
+    };
+  }
+}
+
+
+#endif // !_CENTITY_HANDLER
